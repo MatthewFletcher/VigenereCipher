758ac5426d53c729f9c05f6f9e9c4d3ca384d757
Fletcher <bassmattf@gmail.com>
2020-09-24 12:14:47 -0500

    initial commit of header file

---
+++ decrypt.h
@@ -0,0 +1,21 @@
+#ifndef DECRYPT_H
+#define DECRYPT_H
+int FILE_SIZE;
+char DICTIONARY[DICTLENGTH][MAXWORDLENGTH];
+typedef struct KeyResult
+{
+    char key[4];
+    double percent;
+    char* decrypted;
+} KeyResult_t;
+void initDictionary();
+char* readFile(const char* filename);
+void writeFile(char* filename, char* buff);
+char shiftLetter(char ct, char shift);
+char* decryptStr(char* buffer, char* key);
+int checkWordInDictionary(char* word, int start, int end);
+double getPercentInDict(char* data, char* key);
+void printNode(KeyResult_t* node);
+KeyResult_t** dumbBruteForce(char* DATA);
+void masterDecrypt(const char* filename);
+#endif
